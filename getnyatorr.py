50db3dbc564dd5555ea0339b2792c34056117c64
DarkeErVV <dark_agent7@mail.ru>
2014-07-10 11:49:38 +0400

    Fix, PEP8

--- getnyatorr.py
+++ getnyatorr.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Скрипт для загрузки торрентов мз RSS nyatorrents
-Требуется установленый  торрент-клиент Transmission!
+Скрипт для загрузки торрентов из RSS nyatorrents
+Требуется установленый  торент-клиент Transmission!
 
 """
 
@@ -14,17 +14,17 @@ import shlex
 
 """ Config """
 #path
-main_path    = "/mnt/sdg/python/DlTorrents/"
+main_path    = "/home/user/python/DlTorrents/"
 targets_dir  = "targets"  #папка с целями 
-torrents_dir = "torrents" #папка с торрентами
-status_dir   = "status"   #здесь храним ссылки уже скаченных торрентов  
+torrents_dir = "torrents" #папка с торентами
+status_dir   = "status"   #здесь храним ссылки уже скаченных торентов
 log_dir      = "log"
 
 #Download
-timeout = 2     # таймаут между скачеванием торрентов
+timeout = 2     # таймаут между скачеванием торентов
 
 #Transmission
-trm_login_pass = 'torradmin:torrentforever' #transmission login:password
+trm_login_pass = 'torradmin:torrpass' #transmission login:password
 """end config"""
 
 #Собираем полный путь путь
@@ -34,27 +34,28 @@ status_dir = os.path.join(main_path,status_dir)
 log_dir = os.path.join(main_path,log_dir)
 
 #Настройка логов
-logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = os.path.join(log_dir,'main.log'))
+logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.DEBUG, filename=os.path.join(log_dir, 'main.log'))
+
 
 #
 def run_targets(name,link):
-    logging.info(u'Цель - ' + name + u'  Aдрес - ' + link[:-1])
+    logging.info(u'Цель - ' + name + u' Aдрес - ' + link[:-1])
 
     #проверить есть ли папка под цель
     #if not os.path.isdir(os.path.join(status_dir,name)):
     #    logging.info( u"Не найдена папка для цели - " +name+ u". Создаём...")
     #    os.mkdir(os.path.join(status_dir,name))
 
-    file_rss = os.path.join(status_dir,'rss.xml')
-    file_dl  = os.path.join(status_dir,name)
+    file_rss = os.path.join(status_dir, 'rss.xml')
+    file_dl  = os.path.join(status_dir, name)
 
     if not os.path.isfile(file_dl):
-        f = open(file_dl,'w')
+        f = open(file_dl, 'w')
         f.write('')
         f.close()
 
     #загрузить RSS по ссылке, в файл <имя_цели>/rss.xml
-    urllib.urlretrieve(link,file_rss)
+    urllib.urlretrieve(link, file_rss)
     time.sleep(timeout)
 
     #вытащить ссылки из RSS
@@ -83,54 +84,54 @@ def run_targets(name,link):
 
 
 if not os.path.isdir(targets_dir):
-    logging.critical( u"Ошибка! Не найдена папка с целями! \n Завершаем работу.")
+    logging.critical(u"Ошибка! Не найдена папка с целями! \n Завершаем работу.")
     sys.exit()
 
 if not os.path.isdir(torrents_dir):
-    logging.info( u"Не найдена папка с торентами! Создаём...")
+    logging.info(u"Не найдена папка с торентами! Создаём...")
     os.mkdir(torrents_dir)
 
 if not os.path.isdir(status_dir):
-    logging.info( u"Не найдена папка для храниния иформации о скаченных торентах! Создаём...")
+    logging.info(u"Не найдена папка для храниния иформации о скаченных торентах! Создаём...")
     os.mkdir(status_dir)
 
 if not os.path.isdir(log_dir):
-    logging.info( u"Не найдена папка для записи логов! Создаём...")
+    logging.info(u"Не найдена папка для записи логов! Создаём...")
     os.mkdir(log_dir)
 
-logging.debug( u"Загрузка списка целей ...")
+logging.debug(u"Загрузка списка целей ...")
 for files in os.listdir(targets_dir):
     if not files[0] == '.' and not files[-1] == '~':
-        f = open(os.path.join(targets_dir,files))
+        f = open(os.path.join(targets_dir, files))
         trg_name = files
         trg_link = f.readline()
         f.close()
         dl_link = run_targets(trg_name, trg_link)
 
-        logging.debug( u'Загружена цель: '+ trg_name )
+        logging.debug(u'Загружена цель: ' + trg_name)
         count = 0
         for link in dl_link:
-            logging.debug( u'Загрузка торрента: ' + link )
+            logging.debug(u'Загрузка торрента: ' + link)
             urllib.urlretrieve( link, os.path.join(torrents_dir, trg_name+'-'+str(count)+'.torrent'))
             time.sleep(timeout)
             count += 1
 
-#передаём скаченные торренты в трансмииссион
+#передаём скаченные торенты в трансмииссион
 for fl in os.listdir(torrents_dir):
-    cmd = 'transmission-remote -a '+os.path.join(torrents_dir,fl)+' -n ' + trm_login_pass
+    cmd = 'transmission-remote -a ' + os.path.join(torrents_dir, fl)+' -n ' + trm_login_pass
     args = shlex.split(cmd)
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     result = p.communicate()[0]
 
     #удалить торрент если он успешно добавлен или уже был добавлен
     if result.find('success') != -1:
-       logging.info( u'Торент успешно добавлен '+ fl )
-       os.remove(os.path.join(torrents_dir,fl))
+        logging.info(u'Торент успешно добавлен ' + fl)
+        os.remove(os.path.join(torrents_dir, fl))
     elif result.find('duplicate torrent') != -1:
-       logging.info( u'Торент уже был добавлен! '+ fl )
-       os.remove(os.path.join(torrents_dir,fl))
+        logging.info(u'Торент уже был добавлен! ' + fl)
+        os.remove(os.path.join(torrents_dir, fl))
     elif result.find('invalid') != -1:
-       logging.error( u'Ошибка! Некоректный торент файл! '+ fl )
-       os.remove(os.path.join(torrents_dir,fl))
+        logging.error(u'Ошибка! Некоректный торент файл! ' + fl)
+        os.remove(os.path.join(torrents_dir, fl))
     else:
-       logging.critical( u'Ошибка! Тореннт не добавлен '+ fl )
+        logging.critical(u'Ошибка! Торент не добавлен ' + fl)
